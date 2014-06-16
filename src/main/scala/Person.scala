6eb3b63b8c89099b8c2b64f7df5d5d7d5e466dd9
David Peterson <david@davidseth.net>
2014-06-16 11:47:19 +1000

    adding new Person class

--- src/main/scala/Person.scala
+++ src/main/scala/Person.scala
@@ -1,6 +1,9 @@
 /**
  * Created by david on 16/06/2014.
  */
-class Person {
+package. com.example.model
 
-}
+import java.util.UUID
+import api.DefaultJsonFormats
+
+case class Person ( id: UUID, firstName: String, lastName: String, email: String )
